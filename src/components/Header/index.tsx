6ff8fb61f3b7b5ee4b93959379112920b16d5e07
Luiz Fernando Thomaz <lft.thomaz@gmail.com>
2022-01-25 14:22:31 -0300

    Create to component header
    Create to component header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navegator, Title } from './styles';
+
+const Header: React.FC<{}> = () => {
+  return (
+    <Navegator>
+      <Title>
+        nuffsaid.com Coding Challange
+      </Title>
+    </Navegator>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Navegator = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  border-bottom: 1px solid #000;
+  padding: 10px;
+`;
+
+export const Title = styled.h1`
+  font-size: 22px;
+  font-weight: 500;
+`;
